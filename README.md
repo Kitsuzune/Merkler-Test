b2dfaf46e8808684fe21e37c4ed8b2751fecc3dd
Kitsuzune <teguhwmn189@gmail.com>
2023-09-02 12:29:38 +0700

    second

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # Merkler Test
- 
+
+Detail Project :
+- DB : MySQL
+- Backend : Express
+
+Cara Menjalankan Project
+
+Setup Awal
+1. Clone Github Dari https://github.com/Kitsuzune/Merkler-Test.git
+2. ketikan npm i hingga semua package terinstall
+
+Setup DB
+3. Buat Nama Db "merkler" pada phpmyadmin/xampp/sql server
+   ketikan npx sequelize-cli db:migrate.
+    jika terdapat error karena type module, Buka package.json lalu hapus "type": "module", pada line 12, 
+    Lalu Ketikan npx sequelize-cli db:migrate sekali lagi.
+    Jika berhasil tambahkan kembali type": "module", pada line 12
+
+    Atau Jika tidak mau ribet, export db yang sudah ada di email ke mysql dengan nama db "merkler" pada phpmyadmin/xampp/sql server
+
+Setup Endpoint
+4. Buka Postman atau Sejenisnya Dan Import MErkler.postman_collection.json yang sudah disiapkan di repository
+
+Running Project
+5. Berikut adalah Detail Endpoint Dari Setiap Test
+
+- Soal No 1
+    localhost:5000/merkler/guest -> get
+
+- Soal No 2
+    A. Guest Form - Tambahkan guest baru
+        Metode: POST
+        Endpoint: localhost:5000/merkler/guest
+        Request Body: Data tamu baru, seperti nama, alamat, nomor_telepon, dan catatan. Tampilkan Semua Catatan Tamu
+
+    B. Note Gallery - Tampilkan semua catatan guest
+        Metode: GET
+        Endpoint: localhost:5000/merkler/note-gallery
+        Soal No 2 - Autentikasi
+
+    C. Autentikasi - Login dan Logout
+        Metode: POST
+        Endpoint: localhost:5000/merkler/login
+        Request Body: Data login, seperti username dan password
+
+        Metode: POST   
+        Endpoint: localhost:5000/merkler/logout
+
+    D. Admin - Kelola data guest
+        Metode: GET
+        Endpoint: localhost:5000/merkler/admin
+        Memerlukan autentikasi admin untuk akses.
+        Tampilkan Data Tamu Berdasarkan ID
+
+        Metode: GET
+        Endpoint: localhost:5000/merkler/admin/:id
+        Memerlukan autentikasi admin untuk akses.
+        Gantilah :id dengan ID tamu yang ingin Anda akses.
+        Update Data Tamu Berdasarkan ID
+
+        Metode: PUT
+        Endpoint: localhost:5000/merkler/admin/:id
+        Memerlukan autentikasi admin untuk akses.
+        Gantilah :id dengan ID tamu yang ingin Anda perbarui.
+        Request Body: Data yang ingin Anda perbarui, seperti nama, alamat, nomor_telepon, dan catatan.
+        Hapus Data Tamu Berdasarkan ID
+
+        Metode: DELETE
+        Endpoint: localhost:5000/merkler/admin/:id
+        Memerlukan autentikasi admin untuk akses.
+        Gantilah :id dengan ID tamu yang ingin Anda hapus.
---
+++ merkler.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 02, 2023 at 07:12 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `merkler`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`, `createdAt`, `updatedAt`) VALUES
+(1, 'admin', 'admin', '2023-09-02 06:12:32', '2023-09-02 06:12:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guests`
+--
+
+CREATE TABLE `guests` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `nomor_telepon` varchar(255) NOT NULL,
+  `catatan` text DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guests`
+--
+
+INSERT INTO `guests` (`id`, `nama`, `alamat`, `nomor_telepon`, `catatan`, `createdAt`, `updatedAt`) VALUES
+(2, 'Nama Tamu', 'Alamat Tamu', 'Nomor Telepon Tamu', 'Catatan Tamu', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sequelizemeta`
+--
+
+CREATE TABLE `sequelizemeta` (
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `sequelizemeta`
+--
+
+INSERT INTO `sequelizemeta` (`name`) VALUES
+('20230902035235-create_guest_table.js'),
+('20230902041107-create-admin-table.js');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `guests`
+--
+ALTER TABLE `guests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sequelizemeta`
+--
+ALTER TABLE `sequelizemeta`
+  ADD PRIMARY KEY (`name`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `guests`
+--
+ALTER TABLE `guests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
